cb3c08c66f6aa72af087fa98d39a01eacf7be98d
Pawel Raczyk <pawel.raczyk@vectorsynergy.com>
2023-09-03 13:09:30 +0200

    add template

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,10 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import Home from "./Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Button variant="contained">Hello world</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Home></Home>;
 }
 
 export default App;
---
+++ src/blog-post.1.ts
@@ -0,0 +1,7 @@
+export const post1 = {
+  date: "ds",
+  description: "dsds",
+  image: "dsdsfs",
+  imageLabel: "string",
+  title: "string",
+};
